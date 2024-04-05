9fe6ae3f9493c97b3ec7309bf5378cd7eeb28df6
Mirack <mirackchen@gmail.com>
2024-04-05 12:53:24 -0400

    add feature to select the availeble courses

--- Evaluation/main.js
+++ Evaluation/main.js
@@ -12,37 +12,44 @@ const View = (() => {
     // dom elements
     const domStr = {
         availableCourses: ".course-container",
+        selectedCourses: ".selected-courses .course-container",
     };
 
-    // view of available courses
-    const createAvailableCourses = (courses) => {
-        return courses.map((course, index) => {
-        return `<div class="course-unit ${index % 2 === 0 ? 'even' : ''}">
-                    <p>${course.courseName}<p>
-                    <p>Course Type : ${course.required}</p>
-                    <p>Course Credit : ${course.credit}</p>
+    const createCourseUnit = (course, index) => {
+        return `<div class="course-unit ${index % 2 === 0 ? 'even' : ''}" data-credit="${course.credit}">
+                    <p>${course.courseName}</p>
+                    <p>Course Type: ${course.required}</p>
+                    <p>Course Credit: ${course.credit}</p>
                 </div>`;
-        }).join('');
-    }
+    };
+
+    const createAvailableCourses = (courses) => {
+        return courses.map((course, index) => createCourseUnit(course, index)).join('');
+    };
+
+    const createSelectedCourses = (courses) => {
+        return courses.map((course, index) => createCourseUnit(course, index)).join('');
+    };
 
     // render the view
-    const render = (element,tmp) => {
+    const render = (element, tmp) => {
         element.innerHTML = tmp;
     };
 
     return {
         domStr,
         createAvailableCourses,
+        createSelectedCourses,
         render,
     };
 })();
 
 const Model = ((view, api) => {
     const { getData } = api;
-    const { domStr,createAvailableCourses,render } = view;
+    const { domStr, createAvailableCourses, render } = view;
 
     // state of available courses
-    class availableCoursesState {
+    class AvailableCoursesState {
         constructor() {
             this._availableCourses = [];
         }
@@ -58,43 +65,85 @@ const Model = ((view, api) => {
     }
 
     // state of selected courses
-    class selectedCoursesState {
+    class SelectedCoursesState {
         constructor() {
             this._selectedCourses = [];
+            this.totalCredit = 0;
         }
         get getSelectedCourses() {
             return this._selectedCourses;
         }
         set setSelectedCourses(courses) {
             this._selectedCourses = courses;
-            const selectedCoursesList = document.querySelector(view.domStr.selectedCourses);
-            const tmp = view.createSelectedCourses(this._selectedCourses);
-            console.log(tmp);
-            view.render(selectedCoursesList, tmp);
+            this.totalCredit = this._selectedCourses.reduce((acc, course) => acc + course.credit, 0);
+            const selectedCoursesList = document.querySelector(domStr.selectedCourses);
+            const tmp = createSelectedCourses(this._selectedCourses);
+            render(selectedCoursesList, tmp);
+            this.updateTotalCredit();
+        }
+        updateTotalCredit() {
+            const totalCreditDisplay = document.getElementById("total-credit");
+            totalCreditDisplay.textContent = this.totalCredit;
+        }
+        addCourse(course) {
+            this._selectedCourses.push(course);
+            this.setSelectedCourses = this._selectedCourses;
+        }
+        removeCourse(courseName) {
+            this._selectedCourses = this._selectedCourses.filter((course) => course.courseName !== courseName);
+            this.setSelectedCourses = this._selectedCourses;
+        }
+        clearSelectedCourses() {
+            this._selectedCourses = [];
+            this.totalCredit = 0;
+            this.setSelectedCourses = this._selectedCourses;
         }
     }
 
     return {
-        availableCoursesState,
-        selectedCoursesState,
+        AvailableCoursesState,
+        SelectedCoursesState,
         getData
     };
 })(View, Api);
 
 const Controller = ((view, model) => {
-    const { getData,availableCoursesState,selectedCoursesState} = model;
-    const state = new availableCoursesState();
+    const { domStr } = view;
+    const { getData, AvailableCoursesState, SelectedCoursesState } = model;
+    const availableState = new AvailableCoursesState();
+    const selectedState = new SelectedCoursesState();
+    let isSelectButtonClicked = false;
 
     // initialize the page
-    const init = () =>{
+    const init = () => {
         getData.then((data) => {
             // console.log(data);
-            state.setAvailableCourses = data;
+            availableState.setAvailableCourses = data;
         });
+    }
+
+    const handleCourseSelected = (e) => {
+        const courseUnit = e.target.closest(".course-unit");
+        if (courseUnit && !isSelectButtonClicked) {
+            courseUnit.classList.toggle("selected");
+            const courseData = {
+                courseName: courseUnit.querySelector("p:first-child").textContent,
+                required: courseUnit.querySelector("p:nth-child(2)").textContent.split(": ")[1],
+                credit: parseInt(courseUnit.dataset.credit),
+            };
+
+            if (courseUnit.classList.contains("selected")) {
+                selectedState.addCourse(courseData);
+            } else {
+                selectedState.removeCourse(courseData.courseName);
+            }
+        }
     }
 
     const bootstrap = () => {
         init();
+        const availableCoursesContainer = document.querySelector(view.domStr.availableCourses);
+        availableCoursesContainer.addEventListener("click", handleCourseClick);
     };
 
     return {
