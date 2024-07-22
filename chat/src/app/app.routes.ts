89e5b4ecce7373face95a537c5b4c73e508d7500
Mirack <mirackchen@gmail.com>
2024-07-22 14:55:22 -0400

    fix position bug

--- chat/src/app/app.routes.ts
+++ chat/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ChatComponent } from 'src/components/chat/chat.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 export const routes: Routes = [
     { path: '', component: ChatComponent },
